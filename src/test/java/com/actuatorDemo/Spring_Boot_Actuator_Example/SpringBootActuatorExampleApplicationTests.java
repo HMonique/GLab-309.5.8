4fd70d484c6f66b39305614f4f8f194a4506ada4
HMonique <moniquehl@outlook.com>
2024-09-12 11:21:25 -0400

    first commit

---
+++ src/test/java/com/actuatorDemo/Spring_Boot_Actuator_Example/SpringBootActuatorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actuatorDemo.Spring_Boot_Actuator_Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
